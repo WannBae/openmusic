bf80a2e22b5672382c68ffccdc1c04f519149d01
kiel <kielkobain@gmail.com>
2023-07-18 16:13:07 +0700

    add users migrations

--- migrations/1688680599589_create-table-albums.js
+++ migrations/1688680599589_create-table-albums.js
@@ -13,37 +13,8 @@ exports.up = (pgm) => {
       notNull: true,
     },
   });
-  pgm.createTable("songs", {
-    id: {
-      type: "VARCHAR(50)",
-      primaryKey: true,
-    },
-    title: {
-      type: "TEXT",
-      notNull: true,
-    },
-    year: {
-      type: "INTEGER",
-      notNull: true,
-    },
-    performer: {
-      type: "TEXT",
-      notNull: true,
-    },
-    genre: {
-      type: "TEXT",
-      notNull: true,
-    },
-    duration: {
-      type: "INT",
-    },
-    albumId: {
-      type: "VARCHAR(50)",
-    },
-  });
 };
 
 exports.down = (pgm) => {
   pgm.dropTable("albums");
-  pgm.dropTable("songs");
 };
